8710a2781fe75675234d84eae1992f08df007de2
Louis Rodriguez <louisp285@yahoo.com>
2019-07-13 16:55:43 -0500

    clean art.js

--- models/article.js
+++ models/article.js
@@ -5,30 +5,23 @@ var Schema = mongoose.Schema;
 var ArticleSchema = new Schema({
     title: {
         type: String,
-        required: true,
-        unique: true
+        required: true
     },
-    img: {
+
+    link: {
         type: String,
         required: true
     },
-    link: {
-        type: String,
-        required: true,
-        unique: true
-    },
+
     summary: {
     type: String,
-    required: true,
-    unique: true
-    },
-    note: [{
-        type: Schema.Types.ObjectId,
-        ref: "Note"
-    }]
+    required: true
+    }
 });
 
+    //mongoose method to create schema //
 var Article = mongoose.model("Article", ArticleSchema);
 
+    //exports the article model //
 module.exports = Article;
 
