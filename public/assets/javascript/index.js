221c2e7a28109da7388e45951a2e83bcd56346b5
Louis Rodriguez <louisp285@yahoo.com>
2019-07-14 22:09:54 -0500

    notes

--- public/assets/javascript/index.js
+++ public/assets/javascript/index.js
@@ -19,3 +19,198 @@ var API = {
             type: "GET"
         });
     },
+
+    getArticle: function (id) {
+        return $.ajax({
+            url: "api/articles/" + id,
+            type: "GET"
+        });
+    },
+    save: function (article) {
+        return $.ajax({
+            headers: {
+                "Content-Type": "application/json"
+            },
+            type: "POST",
+            url: "api/saved",
+            data: JSON.stringify(article)
+        });
+    },
+    deleteArticle: function (id) {
+        return $.ajax({
+            url: "api/articles/" + id,
+            type: "DELETE"
+        });
+    },
+    deleteAllScrapedArt: function () {
+        return $.ajax({
+            url: "api/articles/",
+            type: "DELETE"
+        });
+    }
+};
+
+var initArticles = function () {
+    $("#scrapeResults").empty();
+    API.init().then(function (data) {
+        if (data.length > 0) {
+            var $arts = data.map(function (artic) {
+                var $li = $("<li>");
+
+                var saveButton = $("<button>").addClass("btn-small waves-effect waves-light indigo right saveIt").attr("type", "submit").attr("name", "action").text("Save Article");
+                var title = $("<div>").text(artic.title).addClass("collapsible-header");
+
+                var span = $("<a>").attr("href", artic.link).attr("target", "_blank").append($("<span>").text(artic.summary));
+                var body = $("<div>").attr("data-id", artic._id).addClass("collapsible-body").append(span).append(saveButton);
+
+                $li.append(title).append(body);
+
+                return $li;
+            });
+            $("#scrapeResults").append($arts);
+        }
+        else {
+            var $li = $("<li>");
+
+            var title = $("<h5>").text("No More Articles. Get Scraping By Clicking The Button Above!").addClass("center-align");
+
+            $li.append(title);
+            $("#scrapeResults").append($li);
+        }
+    });
+
+};
+
+var newScrape = function () {
+    $("#scrapeResults").empty();
+    API.getScrape().then(function (data) {
+
+        API.init().then(function (data) {
+            if (data.length > 0) {
+                var $arts = data.map(function (artic) {
+                    var $li = $("<li>");
+    
+                    var saveButton = $("<button>").addClass("btn-small waves-effect waves-light indigo right saveIt").attr("type", "submit").attr("name", "action").text("Save Article");
+                    var title = $("<div>").text(artic.title).addClass("collapsible-header");
+    
+                    var span = $("<a>").attr("href", artic.link).attr("target", "_blank").append($("<span>").text(artic.summary));
+                    var body = $("<div>").attr("data-id", artic._id).addClass("collapsible-body").append(span).append(saveButton);
+    
+                    $li.append(title).append(body);
+    
+                    return $li;
+                });
+                $("#scrapeResults").append($arts);
+            }
+            else {
+                var $li = $("<li>");
+    
+                var title = $("<h5>").text("No More Articles. Get Scraping By Clicking The Button Above!").addClass("center-align");
+    
+                $li.append(title);
+                $("#scrapeResults").append($li);
+            }
+        });
+    });
+};
+
+var deleteAllScraped = function () {
+    API.deleteAllScrapedArt().then(function () {
+        $("#scrapeResults").empty();
+        API.init().then(function (data) {
+            if (data.length > 0) {
+                var $arts = data.map(function (artic) {
+                    var $li = $("<li>");
+    
+                    var saveButton = $("<button>").addClass("btn-small waves-effect waves-light indigo right saveIt").attr("type", "submit").attr("name", "action").text("Save Article");
+                    var title = $("<div>").text(artic.title).addClass("collapsible-header");
+    
+                    var span = $("<a>").attr("href", artic.link).attr("target", "_blank").append($("<span>").text(artic.summary));
+                    var body = $("<div>").attr("data-id", artic._id).addClass("collapsible-body").append(span).append(saveButton);
+    
+                    $li.append(title).append(body);
+    
+                    return $li;
+                });
+                $("#scrapeResults").append($arts);
+            }
+            else {
+                var $li = $("<li>");
+    
+                var title = $("<h5>").text("No More Articles. Get Scraping By Clicking The Button Above!").addClass("center-align");
+    
+                $li.append(title);
+                $("#scrapeResults").append($li);
+            }
+        });
+    })
+};
+
+var saveArticle = function () {
+    var id = $(this).parent().attr("data-id");
+
+    API.getArticle(id).then(function (data) {
+        console.log(data);
+        API.save(data).then(function (dataTwo) {
+        });
+        API.deleteArticle(id).then(function (dataThree) {
+            $("#scrapeResults").empty();
+            API.init().then(function (data) {
+                if (data.length > 0) {
+                    var $arts = data.map(function (artic) {
+                        var $li = $("<li>");
+        
+                        var saveButton = $("<button>").addClass("btn-small waves-effect waves-light indigo right saveIt").attr("type", "submit").attr("name", "action").text("Save Article");
+                        var title = $("<div>").text(artic.title).addClass("collapsible-header");
+        
+                        var span = $("<a>").attr("href", artic.link).attr("target", "_blank").append($("<span>").text(artic.summary));
+                        var body = $("<div>").attr("data-id", artic._id).addClass("collapsible-body").append(span).append(saveButton);
+        
+                        $li.append(title).append(body);
+        
+                        return $li;
+                    });
+                    $("#scrapeResults").append($arts);
+                }
+                else {
+                    var $li = $("<li>");
+        
+                    var title = $("<h5>").text("No More Articles. Get Scraping By Clicking The Button Above!").addClass("center-align");
+        
+                    $li.append(title);
+                    $("#scrapeResults").append($li);
+                }
+            });
+        });
+    });
+};
+
+
+
+$.getJSON("/articles", function (data) {
+
+    for (var i = 0; i < data.length; i++) {
+        $("#articles").append("<p data-id='" + data[i]._id + "'>" + data[i].title + "<br />" + data[i].link + "</p>");
+    }
+});
+
+$(document).on("click", "p", function () {
+    $("#notes").empty();
+
+    var thisId = $(this).attr("data-id");
+
+    $.ajax({
+        method: "GET",
+        url: "/articles/" + thisId
+    })
+        .then(function (data) {
+            console.log(data);
+            // The title of the article
+            $("#notes").append("<h2>" + data.title + "</h2>");
+            //input to enter a new title
+            $("#notes").append("<input id='titleinput' name='title' >");
+            //add a new note body
+            $("#notes").append("<textarea id='bodyinput' name='body'></textarea>");
+            //button to submit a new note, with the id of the article saved to it
+            $("#notes").append("<button data-id='" + data._id + "' id='savenote'>Save Note</button>");
+
